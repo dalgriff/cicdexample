e54463ca040524f8eb2f51a6d2977be443c097ea
dalgriff <dgriffin@2u.com>
2023-03-20 22:50:17 -0400

    profile

--- hello-circle/src/test/java/com/company/hellocircle/controller/HelloCircleControllerTest.java
+++ hello-circle/src/test/java/com/company/hellocircle/controller/HelloCircleControllerTest.java
@@ -17,7 +17,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HelloCircleController.class)
-@Profile("test")
 public class HelloCircleControllerTest {
 
     @Autowired
