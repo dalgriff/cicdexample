4cf4ed840d7e1ee5802ffea7781ae29828e5ddde
dalgriff <dgriffin@2u.com>
2023-03-20 20:33:57 -0400

    refactor

--- hello-circle/src/test/java/com/company/hellocircle/controller/HelloCircleControllerTest.java
+++ hello-circle/src/test/java/com/company/hellocircle/controller/HelloCircleControllerTest.java
@@ -26,7 +26,7 @@ public class HelloCircleControllerTest {
         // arrange and act
         mockMvc.perform(get("/answer"))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().is3xxRedirection());
 
                 // assert
               //  .andExpect(content().string("The answer is 42!"));
